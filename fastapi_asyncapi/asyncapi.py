51ed532a1c7e0d00e73737b74a11577096b56bef
Marcelo Trylesinski <marcelotryle@gmail.com>
2021-04-16 04:59:03 +0200

    more

--- fastapi_asyncapi/asyncapi.py
+++ fastapi_asyncapi/asyncapi.py
@@ -1,10 +1,13 @@
-from typing import Dict, List, Literal, Optional, TypedDict, Union
+from typing import Any, Dict, List, Literal, Optional, Sequence, Union
 
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from fastapi.openapi.utils import get_openapi
 from fastapi.responses import HTMLResponse
-from pydantic.networks import AnyHttpUrl, EmailStr
-
-get_openapi()
+from fastapi.routing import APIRoute
+from pydantic import AnyHttpUrl, BaseModel, EmailStr
+from starlette.routing import BaseRoute
+from typing_extensions import TypedDict
 
 
 class Contact(TypedDict):
@@ -66,20 +69,22 @@ HTTPMethod = Literal[
     "GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS", "CONNECT", "TRACE"
 ]
 
+Schema = Dict[str, Any]
+
 
 class HTTPOperationBinding(TypedDict):
     type: Literal["request", "response"]
     method: HTTPMethod
-    # query: # Missing this
-    bindingVersion: Union[Literal["latest"], str]
+    query: Optional[Schema]
+    bindingVersion: Optional[Union[Literal["latest"], str]]
 
 
+# TODO: Missing other bindings!
 class OperationBinding(TypedDict):
     http: Optional[HTTPOperationBinding]
-    # Missing remaining
 
 
-Bindings = Dict[str, OperationBinding]
+Bindings = List[Dict[str, OperationBinding]]
 
 
 class Operation(TypedDict):
@@ -95,11 +100,29 @@ Subscribe = Operation
 Publish = Operation
 
 
-class Channel(TypedDict):
-    ref: Optional[str]  # alias="$ref"
+class Parameter(TypedDict):
     description: Optional[str]
-    subscribe: Optional[Subscribe]
-    publish: Optional[Publish]
+    schema: Optional[Schema]
+    location: Optional[str]
+
+
+Reference = TypedDict("Reference", {"$ref": str})
+
+
+Parameters = List[Dict[str, Union[Parameter, Reference]]]
+
+
+Channel = TypedDict(
+    "Channel",
+    {
+        "$ref": Optional[str],
+        "description": Optional[str],
+        "subscribe": Optional[Subscribe],
+        "publish": Optional[Publish],
+        "parameters": Optional[Parameters],
+        "bindings": Optional[Bindings],
+    },
+)
 
 
 Security = Dict[str, List[str]]
@@ -116,24 +139,52 @@ class Server(TypedDict):
     channels: Channels
 
 
-class AsyncAPI(TypedDict):
+class AsyncAPI(BaseModel):
     asyncapi: str
-    id: str
+    id: Optional[str]
     info: Info
-    servers: Optional[List[Dict[str, Server]]]  # Validate str on Dict[str, ...]
+    servers: Optional[List[Dict[str, Server]]]
+    channels: Optional[Channels]
+    # components:
+    tags: Optional[List[Tag]]
+    externalDocs: Optional[ExternalDocumentation]
 
 
 def get_asyncapi(
     *,
     title: str,
     version: str,
+    routes: Sequence[BaseRoute],
+    asyncapi_version: str = "2.0.0",
+    id: Optional[str] = None,
     description: Optional[str] = None,
     termsOfService: Optional[AnyHttpUrl] = None,
     contact: Optional[Contact] = None,
+    license: Optional[License] = None,
     tags: Optional[List[Tag]] = None,
-    # server: Optional[List[Server]] = None,
-):
-    ...
+    servers: Optional[List[Server]] = None,
+) -> Dict[str, Any]:
+    info = Info(
+        title=title,
+        version=version,
+        description=description,
+        termsOfService=termsOfService,
+        contact=contact,
+        license=license,
+    )
+    channels: Channels = []
+    for route in routes:
+        if isinstance(route, APIRoute):
+            channels.append()
+            print(route.endpoint)
+
+    return jsonable_encoder(
+        AsyncAPI(
+            asyncapi=asyncapi_version, id=id, info=info, tags=tags, servers=servers
+        ),
+        by_alias=True,
+        exclude_none=True,
+    )
 
 
 def get_asyncapi_html(
@@ -162,3 +213,18 @@ def get_asyncapi_html(
     </html>
     """
     return HTMLResponse(html)
+
+
+app = FastAPI(title="MyAPI", version="1.0.0", docs_url=None)
+
+
+@app.get("/asyncapi.json")
+async def asyncapi_json():
+    return get_asyncapi(title=app.title, version=app.version, routes=app.routes)
+
+
+@app.get("/docs")
+async def asyncapi_docs():
+    return get_asyncapi_html(
+        asyncapi_url="http://localhost:8000/asyncapi.json", title=app.title
+    )
