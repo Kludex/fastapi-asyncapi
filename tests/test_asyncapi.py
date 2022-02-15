1fdf71f753c9197a1c156e9e2fb0dc259be09cff
djapy <djapy@yandex.ru>
2022-02-15 09:43:17 +0300

    Fixed lints

--- tests/test_asyncapi.py
+++ tests/test_asyncapi.py
@@ -6,8 +6,8 @@ from fastapi_asyncapi.asyncapi import get_asyncapi
 
 def test_get_asyncapi():
 
-    path = '/test/path/'
-    title = 'My async api'
+    path = "/test/path/"
+    title = "My async api"
     version = "2.3.0"
     routes = [APIRoute(path, lambda x: 1)]
     result = get_asyncapi(title=title, version=version, routes=routes)
